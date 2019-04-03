597a07ed0ba63f63f1fbe27ead8b7bc2ca6a634b
flotea <hgrimmer0786@myhta.org>
2019-04-03 11:28:09 -1000

    VC 4/3/19

---
+++ app.js
@@ -0,0 +1,8 @@
+window.onload= function(){
+    var dirt = document.getElementsByClassName("dirt");
+    var sound = document.getElementById("sound");
+    var grid = document.getElementById("dirt-box");
+    dirt.addEventListener("click",)
+    
+    var mole = document.getElementById("mole");
+}
