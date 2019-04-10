bc93836842c6ce98a8ddf6ad32650903941b8799
flotea <hgrimmer0786@myhta.org>
2019-04-09 18:53:42 -1000

    Finished JS, Messing w/ CSS

--- app.js
+++ app.js
@@ -1,8 +1,41 @@
 window.onload= function(){
     var dirt = document.getElementsByClassName("dirt");
     var sound = document.getElementById("sound");
+    var sound2 = document.getElementById("sound2");
     var grid = document.getElementById("dirt-box");
-    dirt.addEventListener("click",)
-    
-    var mole = document.getElementById("mole");
+    var scoreboard = document.getElementById("scoreboard");
+    for(let i=0;i<dirt.length;i++ ){
+        dirt[i].addEventListener("click", function clickMole(){
+        var score = 0;
+        if (dirt[i].innerHTML){
+            sound.play();
+            dirt[i].innerHTML = "";
+
+            }
+        else{
+                sound2.play();
+                console.log ("nothing is there");
+            }
+            }
+        )
+    }
+
+    setInterval(function setMole(){
+        var mole = document.createElement("DIV");
+        mole.setAttribute("id", "mole");
+        var rando = Math.floor(Math.random() * Math.floor(dirt.length));
+
+        if (dirt[rando].innerHTML == ""){
+            dirt[rando].appendChild(mole);
+        }
+        else{
+            console.log("there are too many moles!");
+        }
+
+    }, 3000);
+
+
+
+
 }
+
