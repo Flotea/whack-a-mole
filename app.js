3499f9dca2bd2130ff767f8083b4fd2b23276140
flotea <hgrimmer0786@myhta.org>
2019-04-17 12:13:41 -1000

    Added score, timer, and extra things

--- app.js
+++ app.js
@@ -3,22 +3,48 @@ window.onload= function(){
     var sound = document.getElementById("sound");
     var sound2 = document.getElementById("sound2");
     var grid = document.getElementById("dirt-box");
-    var scoreboard = document.getElementById("scoreboard");
+    var sidemole=document.getElementById("sideMole");
+    var reset = document.getElementById("reset");
+    var time = 60;
+
+    var score = 0;
     for(let i=0;i<dirt.length;i++ ){
         dirt[i].addEventListener("click", function clickMole(){
-        var score = 0;
+        
         if (dirt[i].innerHTML){
             sound.play();
             dirt[i].innerHTML = "";
-
+            score++;
+            document.getElementById("score").innerHTML = "Score: " + score;
+            document.getElementById("timer").innerHTML = "Time: " + time;
             }
         else{
                 sound2.play();
                 console.log ("nothing is there");
+                score-=1;
+                document.getElementById("score").innerHTML = "Score: " + score;
             }
+
             }
         )
     }
+
+    sidemole.addEventListener("click", function wrong(){
+        sound2.play();
+        score -=600;
+        document.getElementById("score").innerHTML = "Score: " + score;
+
+    })  
+
+    reset.addEventListener("click", function res(){
+        score=0;
+        document.getElementById("score").innerHTML = "";
+        time=60;
+
+    })
+
+        
+
 
     setInterval(function setMole(){
         var mole = document.createElement("DIV");
@@ -27,12 +53,27 @@ window.onload= function(){
 
         if (dirt[rando].innerHTML == ""){
             dirt[rando].appendChild(mole);
+            console.log("working");
         }
         else{
             console.log("there are too many moles!");
         }
 
     }, 3000);
+
+    setInterval(function timer(){
+        if (time==0){
+            document.getElementById("timer").innerHTML = "Game Over. You have " + score + " points, loser.";
+            
+        }
+        else {
+            time -=1;
+            console.log(time);
+            document.getElementById("timer").innerHTML = "Time: " + time;
+
+        }
+
+    }, 1000)
 
 
 
